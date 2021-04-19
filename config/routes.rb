44fd4cb8cf247ccfcc484546c595f2e3f475ec5b
santdev404 <santdev404@gmail.com>
2021-04-19 14:06:53 -0500

    Devisa extra config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :courses
   devise_for :users
-  root 'static_pages#landing_page'
+  resources :courses
+  resources :users, only: [:index]
 
+  root 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 
 end
