e429d70fa946fe397dd2ab34f22ea6a0b474f834
santdev404 <santdev404@gmail.com>
2021-04-18 20:01:22 -0500

    Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :courses
+  devise_for :users
   root 'static_pages#landing_page'
 
   get 'privacy_policy', to: 'static_pages#privacy_policy'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 const webpack = require("webpack")
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
     $: 'jquery',
@@ -6,4 +8,6 @@ environment.plugins.append("Provide", new webpack.ProvidePlugin({
     'window.jQuery': 'jquery',
     Popper: ['popper.js', 'default']
   }))
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
