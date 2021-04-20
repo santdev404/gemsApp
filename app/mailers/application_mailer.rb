3f71947cda312ee9f4acafc07fa914959613a938
santdev404 <santdev404@gmail.com>
2021-04-20 15:43:41 -0500

    Add email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'dev404m@gmail.com'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+
+  port: 587,
+  address: 'smtp.gmail.com',
+  user_name: 'dev404m@gmail.com',
+  password: 'glcdpgzlxnzbevwp',
+  authentication: :plain,
+  enable_starttls_auto: true
+  
+}
